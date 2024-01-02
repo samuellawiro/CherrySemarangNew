19259d04f8dd295702e87f374611eae776677421
samuellawiro <86443849+samuellawiro@users.noreply.github.com>
2024-01-02 23:55:15 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <div className = "navbarWrapper">
-        <img src={logoCherry}></img>
+
         <h1>With Cherry With Love</h1>
         <ul>
           <li>About</li>
